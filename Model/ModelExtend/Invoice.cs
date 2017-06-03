904103d141ac288fc7016dde9329d67c57905bde
Carlos Ernesto Cordoba <carlosc220@hotmail.com>
2017-06-03 07:18:05 -0500

    factura

---
+++ Model/ModelExtend/Invoice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    partial class Invoice
+    {
+        public string CustomerName { get; set; }
+    }
+}
